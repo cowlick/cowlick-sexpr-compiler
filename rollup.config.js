5ddc11ae083138f311998a9d40d82b85b5714db3
pocketberserker <pocketberserker@gmail.com>
2018-12-23 20:40:51 +0900

    re-implement cli

--- rollup.config.js
+++ rollup.config.js
@@ -20,6 +20,7 @@ export default {
   ],
   external: [
     "@cowlick/analyzer",
+    "commandpost",
     "fs",
     "path"
   ]
---
+++ src/cli.ml
@@ -0,0 +1,29 @@
+open Commandpost
+
+type root_options = <
+  out: string;
+> Js.t
+
+type root_args = <
+  entry: string;
+> Js.t
+
+let root: (root_options, root_args) Command.t =
+  create "cowlick-sexpr-compiler <entry>"
+  |. Command.version ~version:"" ~flags:"-v, --version"
+  |. Command.help "-h, --help" "print this help"
+  |. Command.option ~flags:"-o, --out" ~description:"output directory" ~default_value:"script" ()
+  |. Command.action (fun opts args _ ->
+    let current = Sys.getcwd () in
+    let out = Node.Path.join [|current; opts##out|] in
+    args##entry
+    |> Filename.concat current
+    |> Compiler.compile out
+  );;
+
+root
+|. exec Sys.argv
+|> Js.Promise.catch (fun e ->
+  Js.log(e);
+  Js.Promise.resolve ()
+);;
